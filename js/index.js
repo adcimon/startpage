d7c4f9260fa3915126331594f47de74af4b4f7f7
adcimon <37084114+adcimon@users.noreply.github.com>
2021-12-04 16:39:43 +0100

    Added tab functionality

--- js/index.js
+++ js/index.js
@@ -1,29 +1,46 @@
 "use strict";
 
-import { Links } from "./links.js";
-
 function main()
 {
     let container = document.querySelector(".item-container");
 
+    let tabindex = 3;
     Links.forEach(link =>
     {
-        let cell = createCell();
+        let cell = createCell(tabindex);
 
         let a = cell.querySelector("a");
-        a.style.backgroundColor = link.color;
+        a.style.background = link.background;
         a.href = link.url;
+        a.target = link.target;
 
         let img = a.querySelector("img");
         img.src = link.icon;
 
         container.appendChild(cell);
+
+        tabindex++;
+    });
+
+    let focusguard1 = document.querySelector("#focusguard1");
+    focusguard1.addEventListener("focus", () =>
+    {
+        let last = document.querySelector(`[tabindex="${tabindex - 1}"]`);
+        last.focus();
+    });
+
+    let focusguard2 = document.querySelector("#focusguard2");
+    focusguard2.tabIndex = tabindex;
+    focusguard2.addEventListener("focus", () =>
+    {
+        let first = document.querySelector(`[tabindex="2"]`);
+        first.focus();
     });
 }
 
-function createCell()
+function createCell( tabindex )
 {
-    let html = '<div class="item-cell"><a class="item"><img/></a></div>';
+    let html = `<div class="item-cell"><a class="item" tabindex="${tabindex}"><img/></a></div>`;
     let template = document.createElement("template");
     template.innerHTML = html;
 
--- js/links.js
+++ js/links.js
@@ -1,45 +1,59 @@
-export const Links =
+const Links =
 [
     {
-        "name":     "GitHub",
-        "icon":     "./icons/github.svg",
-        "color":    "hsla(213, 11%, 16%, 1)",
-        "url":      ""
+        "name":         "New Window",
+        "icon":         "./icons/plus.svg",
+        "background":   "linear-gradient(to right, hsla(190, 100%, 43%, 1), hsla(195, 100%, 34%, 1))",
+        "url":          "#",
+        "target":       "_blank"
     },
     {
-        "name":     "Twitter",
-        "icon":     "./icons/twitter.svg",
-        "color":    "hsla(203, 89%, 53%, 1)",
-        "url":      ""
+        "name":         "YouTube",
+        "icon":         "./icons/youtube.svg",
+        "background":   "hsla(0, 100%, 50%, 1)",
+        "url":          "https://www.youtube.com/",
+        "target":       "_self"
     },
     {
-        "name":     "YouTube",
-        "icon":     "./icons/youtube.svg",
-        "color":    "hsla(0, 100%, 50%, 1)",
-        "url":      ""
+        "name":         "GitHub",
+        "icon":         "./icons/github.svg",
+        "background":   "hsla(213, 11%, 16%, 1)",
+        "url":          "https://github.com/adcimon",
+        "target":       "_self"
     },
     {
-        "name":     "Google Drive",
-        "icon":     "./icons/gdrive.svg",
-        "color":    "hsla(255, 100%, 100%, 1)",
-        "url":      ""
+        "name":         "WhatsApp",
+        "icon":         "./icons/whatsapp.svg",
+        "background":   "hsla(130, 76%, 41%, 1)",
+        "url":          "https://web.whatsapp.com/",
+        "target":       "_self"
     },
     {
-        "name":     "Google Mail",
-        "icon":     "./icons/gmail.svg",
-        "color":    "hsla(255, 100%, 100%, 1)",
-        "url":      ""
+        "name":         "Twitter",
+        "icon":         "./icons/twitter.svg",
+        "background":   "hsla(203, 89%, 53%, 1)",
+        "url":          "https://twitter.com/adcimon",
+        "target":       "_self"
     },
     {
-        "name":     "Google Maps",
-        "icon":     "./icons/gmaps.svg",
-        "color":    "hsla(255, 100%, 100%, 1)",
-        "url":      ""
+        "name":         "Google Mail",
+        "icon":         "./icons/gmail.svg",
+        "background":   "hsla(255, 100%, 100%, 1)",
+        "url":          "https://www.google.com/mail/",
+        "target":       "_self"
     },
     {
-        "name":     "Firefox",
-        "icon":     "./icons/firefox.svg",
-        "color":    "hsla(255, 100%, 100%, 1)",
-        "url":      ""
+        "name":         "Google Drive",
+        "icon":         "./icons/gdrive.svg",
+        "background":   "hsla(255, 100%, 100%, 1)",
+        "url":          "https://www.google.com/drive/",
+        "target":       "_self"
+    },
+    {
+        "name":         "Google Maps",
+        "icon":         "./icons/gmaps.svg",
+        "background":   "hsla(255, 100%, 100%, 1)",
+        "url":          "https://www.youtube.com/",
+        "target":       "_self"
     }
 ];
