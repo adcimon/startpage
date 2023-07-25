0c6b32023c01b53825a7c99803436439919bf94f
adcimon <37084114+adcimon@users.noreply.github.com>
2023-07-25 13:18:14 +0200

    Refactored folders

---
+++ scripts/links.js
@@ -0,0 +1,44 @@
+const Links = [
+	{
+		name: 'New Window',
+		icon: './icons/favicon/favicon.svg',
+		background: 'hsla(217, 19%, 38%, 1)',
+		url: '#',
+		target: '_blank',
+	},
+	{
+		name: 'YouTube',
+		icon: './icons/youtube.svg',
+		background: 'hsla(0, 100%, 50%, 1)',
+		url: 'https://www.youtube.com/',
+		target: '_self',
+	},
+	{
+		name: 'GitHub',
+		icon: './icons/github.svg',
+		background: 'hsla(213, 11%, 16%, 1)',
+		url: 'https://github.com',
+		target: '_self',
+	},
+	{
+		name: 'Google Mail',
+		icon: './icons/gmail.svg',
+		background: 'hsla(255, 100%, 100%, 1)',
+		url: 'https://www.google.com/mail',
+		target: '_self',
+	},
+	{
+		name: 'Google Drive',
+		icon: './icons/gdrive.svg',
+		background: 'hsla(255, 100%, 100%, 1)',
+		url: 'https://drive.google.com/drive/my-drive',
+		target: '_self',
+	},
+	{
+		name: 'Google Maps',
+		icon: './icons/gmaps.svg',
+		background: 'hsla(255, 100%, 100%, 1)',
+		url: 'https://www.google.com/maps',
+		target: '_self',
+	},
+];
